fb5dee7b6fc5cc8aa408341bcb045d15c7462992
AIM <adeshinaibrahim571@gmail.com>
2026-06-01 13:16:56 +0100

    fix: cookie disappearing issue

--- apps/server/src/utils/tokens.ts
+++ apps/server/src/utils/tokens.ts
@@ -6,17 +6,19 @@ import bcrypt from 'bcryptjs';
 const ACCESS_TOKEN_MAX_AGE_MS = 15 * 60 * 1000;
 const REFRESH_TOKEN_MAX_AGE_MS = 7 * 24 * 60 * 60 * 1000;
 
+const isProd = env.NODE_ENV === 'production'
+
 export const sendToken = (res: Response, accessToken: string, refreshToken: string) => {
     res.cookie('accessToken', accessToken, {
         httpOnly: true,
-        secure: env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'strict',
         maxAge: ACCESS_TOKEN_MAX_AGE_MS,
     });
     res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
-        secure: env.NODE_ENV === 'production',
-        sameSite: 'strict',
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'strict',
         maxAge: REFRESH_TOKEN_MAX_AGE_MS,
     });
 };
